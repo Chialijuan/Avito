406fa3338e32313f50f24f758c4741b8bc667955
juan <chialijuan303@gmail.com>
2017-09-18 14:48:15 +0800

    Adding description_2vec notebooks

--- description_2vec.py
+++ description_2vec.py
@@ -10,7 +10,7 @@ import string
 from stop_words import get_stop_words
 import regex as re
 from contextlib import closing
-
+import timeit
 
 pd.options.display.max_colwidth = -1
 pd.options.display.max_columns = None
@@ -28,7 +28,7 @@ def remove_punctuation(text):
 
 def clean_text(doc):
     try:
-        docs = remove_punctuation(doc)
+        docs = remove_punctuation(doc).replace('\n', ' ')
         docs = docs.lower().split(' ')
         return [tok for tok in docs if tok not in ru_stopwords]
     except Exception as e :
@@ -40,13 +40,21 @@ item_info['description']=item_info['description'].fillna('None')
 
 description = item_info['description']
 
-pool = ThreadPool(4)
-item_info['description_clean'] = pool.map(clean_text,description)
-print(item_info.head())
+start_time = timeit.default_timer()
+item_info['description_clean'] = item_info['description'].apply(clean_text)
+
+#pool = ThreadPool(4)
+#item_info['description_clean'] = pool.map(clean_text,description)
+print(item_info.tail())
+
 #with closing(mp.Pool(3)) as p:
 #    item_info['description_clean'] = p.imap(clean_text,description,10)
 #    p.terminate()
 
+elapsed = timeit.default_timer()-start_time
+print('Time elapsed: {}'.format(elapsed))
+# Time Elapsed for Thread: 1118.923s = 19 mins
+# Time Elapsed for .apply: Killed
 
-item_info.to_csv('ItemInfo_train2.csv', encoding='utf-8')
-print('Saved to ItemInfo_train2.csv')
+#item_info.to_csv('ItemInfo_train2.csv', encoding='utf-8', index=False)
+#print('Saved to ItemInfo_train2.csv')
