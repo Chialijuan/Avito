615ca9e7e5ac670098ad6bcf979e3b76106eecde
juan <chialijuan303@gmail.com>
2017-09-19 08:51:24 +0800

    Added description_2vec.model

--- description_2vec.py
+++ description_2vec.py
@@ -1,67 +1,26 @@
-
-# coding: utf-8
-
-from __future__ import unicode_literals
-
-#import multiprocessing as mp
-from multiprocessing.dummy import Pool as ThreadPool
 import pandas as pd
-import string
-from stop_words import get_stop_words
-import regex as re
-from contextlib import closing
 import timeit
-
-pd.options.display.max_colwidth = -1
-pd.options.display.max_columns = None
+from gensim import models, similarities
 
 filepath = '~/Documents/Avito/'
-item_info = pd.read_csv(filepath+'ItemInfo_train.csv',encoding='utf-8')
 
-print('Length of df: {}'.format(len(item_info)))
+df = pd.read_csv(filepath+'ItemInfo_train2.csv', encoding='utf-8', converters={"description_clean": lambda x: x.strip("[]").split(", ")})
 
-# Russian Stopwords
-ru_stopwords = get_stop_words('russian')
+del df['Unnamed: 0']
 
-def remove_punctuation(text):
-    return re.sub(ur"\p{P}+", "", text)
+description = df['description_clean'].tolist()
 
-def clean_text(doc):
-    try:
-        docs = remove_punctuation(doc).replace('\n', ' ')
-        docs = docs.lower().split(' ')
-        result =  [tok for tok in docs if tok not in ru_stopwords]
-        print(type(result))
-        return result
-    except Exception as e :
-        print(doc)
-        print(e)
-        pass
-
-item_info['description']=item_info['description'].fillna('None')
-
-description = item_info['description']
+print('No. of rows:{}'.format(len(description)))
 
 start_time = timeit.default_timer()
 
-# pandas .apply
-#item_info['description_clean'] = item_info['description'].apply(clean_text)
+model = models.word2vec.Word2Vec(description, sg=1, size=75, window =5,
+                                         min_count=1, workers=3,seed=1)
 
-# MultiThreading
-pool = ThreadPool(4)
-item_info['description_clean'] = pool.map(clean_text,description)
-print(type(item_info['description_clean'][0]))
+model2 = model.wv
+del model
 
-# MultiPooling
-#with closing(mp.Pool(3)) as p:
-#    item_info['description_clean'] = p.imap(clean_text,description,10)
-#    p.terminate()
+model2.save('description_2vec.model')
 
-print(item_info.tail())
 elapsed = timeit.default_timer()-start_time
 print('Time elapsed: {}'.format(elapsed))
-# Time Elapsed for Thread: 1118.923s = 19 mins
-# Time Elapsed for .apply: Killed
-
-#item_info.to_csv('ItemInfo_train2.csv', encoding='utf-8', index=False)
-#print('Saved to ItemInfo_train2.csv')
